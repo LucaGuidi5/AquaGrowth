86a616cdb7d4dea62e05f22244c9fc8764ab4320
LucaGuidi5 <luca.drives@gmail.com>
2025-04-28 15:16:29 +0200

    .

--- utility.js
+++ utility.js
@@ -31,8 +31,43 @@ function initializeMap(){
     }
 }
 
+function metersToPixels(meters, lat, zoom) {
+  const earthCircumference = 40075017; // in metri
+  const latitudeRadians = lat * (Math.PI / 180);
+  const metersPerPixel = earthCircumference * Math.cos(latitudeRadians) / Math.pow(2, zoom + 8);
+  return meters / metersPerPixel;
+}
+
+
+function addLayer(){
+ 
+  var heatData = [
+    [45.4408, 12.3155, 0.01], // Venezia centro
+    [45.4378, 12.3150, 0.4],
+    [45.4418, 12.3185, 0.7],
+    [45.4438, 12.3205, 1],
+    // Puoi aggiungere quanti punti vuoi
+  ];
+
+  var currentZoom = map.getZoom();
+  var centerLat = 45.4408; // puoi anche calcolare la media dei tuoi punti
+  var radiusInPixels = metersToPixels(5000, centerLat, currentZoom);
+
+  var heat = L.heatLayer(heatData, {
+    radius: radiusInPixels,
+    blur: -15,
+    maxZoom: 0,
+    gradient: {
+        //0.0: 'red',    // intensità 0 -> rosso
+        //0.5: 'yellow', // intensità 0.5 -> giallo
+        1.0: 'green'   // intensità 1 -> verde
+    }
+  }).addTo(map);
+}
+
 window.readData = readData;
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeMap();
+  addLayer();
 });
